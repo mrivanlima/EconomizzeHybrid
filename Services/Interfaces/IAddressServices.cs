42546a19596947f4a81f4b09f1df944677157cbe
TheKauanLima <thekauanlima@gmail.com>
2024-09-04 19:08:22 -0400

    Added Address Cache Implementation to store and pull data from cache when user inputs data.

--- Services/Interfaces/IAddressServices.cs
+++ Services/Interfaces/IAddressServices.cs
@@ -10,7 +10,7 @@ namespace EconomizzeHybrid.Services.Interfaces
     public interface IAddressServices
     {
         Task SearchZipCodeAsync(SearchZipCodeModel CurrentZipCode);
-
+        Task ReadAsyncById(int id);
         Task CreateUserAddressAsync(AddressModel model);
         AddressModel CurrentAddress { get; set; }
         SearchZipCodeModel CurrentZipCode { get; set; }
